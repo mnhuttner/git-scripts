9b78efd0184d735b2bccbdf46bf662cd9c74de48
mnhuttner <mhuttner@gmail.com>
2017-02-05 00:16:58 -0500

    initialization

--- init.sh
+++ init.sh
@@ -1,5 +1,13 @@
 set -x
+
+ssh-add ~/.ssh/id_rsa
+eval $(ssh-agent -s)
+ssh -T git@github.com
+
 git config --global user.name 'mnhuttner'
 git config --global user.email 'mhuttner@gmail.com'
 git config --system core.editor vi
 
+git remote add origin git@github.com:mnhuttner/git-scripts
+git pull https://mnhuttner:Nojoker123@github.com/mnhuttner/git-scripts
+git push -u origin master
